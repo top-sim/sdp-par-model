524b1a338d14d779058da356c7c97c267bbd4a1f
Bojan Nikolic <bojan@bnikolic.co.uk>
2014-11-14 12:46:45 +0000

    Delete old support awk scripts and update README. Emaxima etc was not used in preparation of M7

--- dump_maxima_code.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-awk -f dump_maxima_code.awk MajCycleModel.tex    > MajCycleModel.wxm
-awk -f dump_maxima_code.awk DataFlow.tex         > DataFlow.wxm
-awk -f dump_maxima_code.awk CompHWPowerModel.tex > CompHWPowerModel.wxm
