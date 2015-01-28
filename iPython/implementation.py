fad27db8f62d0889a99ac2d6daeaa75b7307ef10
Francois Malan <fmalan@medvis.org>
2015-01-29 00:39:32 +0200

    Reviving all the old blocks of code

--- iPython/implementation.py
+++ iPython/implementation.py
@@ -38,15 +38,15 @@ def optimize_expr(expression, free_var, bound_lower, bound_upper):
             pass
         return result.x
     
-def calc_tel_params(band=None, mode=None, hpso=None):
+def calc_tel_params(band=None, mode=None, hpso_key=None):
     '''
     Calculate telescope parameters for a supplied band, mode or hpso
     '''
     freq_range = {}
     hpso_params = None
-    if hpso is not None:
-        assert hpso in hpsos
-        hpso_params = hpsos[hpso]
+    if hpso_key is not None:
+        assert hpso_key in hpsos
+        hpso_params = hpsos[hpso_key]
         assert hpso_params is not None
         assert 'telescope' in hpso_params
         telescope = hpso_params['telescope']
@@ -113,10 +113,7 @@ def substitute_parameters_binned(expression, tp, bins, counts, nbins_used, verbo
         temp_result += expression.subs(tp).subs(tp)
     return temp_result
 
-def evaluate_expression(expression, telescope_parameters, mode=None, verbose_variables=()):
-    if mode == 'CS':
-        raise Exception('Cannot yet handle CS mode when using binned baselines. Should be simple to implement though.')
-
+def evaluate_expression(expression, telescope_parameters, verbose_variables=()):
     tp = telescope_parameters.copy()  # Make a copy, as we will be locally modifying the dictionary
     bins = tp.pop(baseline_bins) # Remove the array of baselines from the parameter dictionary
     counts = tp.pop(baseline_bin_counts) # Remove the array of baselines from the parameter dictionary
@@ -160,19 +157,20 @@ def minimize_expression(expression, telescope_parameters, mode=None, verbose=Fal
         print "Tsnap has been optimized as : %f, yielding a minimum value of %f Peta-units" % (Tsnap_optimal, value_optimal / 1e15)
     return {Tsnap : Tsnap_optimal, 'value' : value_optimal}  # Replace Tsnap with its optimal value
 
-def find_optimal_Tsnap_Nfacet(chosen_band, chosen_mode, max_number_nfacets=200, verbose=False):
+def find_optimal_Tsnap_Nfacet(band=None, mode=None, hpso=None, max_number_nfacets=200, verbose=False):
     '''
     Computes the optimal value for Tsnap and Nfacet that minimizes the value of Rflop for a given telescope, band and mode
     Returns result as a 2-tuple (Tsnap_opt, Nfacet_opt)
     '''
     if verbose:
-        print 'Finding optimal values for Tsnap and Nfacet for (%s, %s)' % (chosen_band, chosen_mode)
+        if hpso is None:
+            print 'Finding optimal values for Tsnap and Nfacet for (band = %s, mode = %s)' % (band, mode)
+        else:
+            print 'Finding optimal values for Tsnap and Nfacet for HPSO %s (mode = %s)' % (hpso, mode)
     
     flop_results = {} # Maps nfacet values to flops
-    optimal_results = {}
     flop_array = []
     Tsnap_array = []
-    minimum_val = sys.float_info.max
     warned = False
     for nfacets in range(1, max_number_nfacets+1):  # Loop over the different integer values of NFacet (typically 1..10)
         # Warn if large values of nfacets are reached, as it may indicate an error and take long!
@@ -184,13 +182,13 @@ def find_optimal_Tsnap_Nfacet(chosen_band, chosen_mode, max_number_nfacets=200, 
         if verbose:
             print 'Evaluating Nfacets = %d' % nfacets
         universal[Nfacet] = nfacets
-        pp = calc_tel_params(band=chosen_band, mode=chosen_mode)
-        answer = minimize_expression(expression=Rflop, telescope_parameters=pp, mode=chosen_mode)
+        parameters = calc_tel_params(band=band, mode=mode, hpso_key=hpso)
+        answer = minimize_expression(expression=Rflop, telescope_parameters=parameters, mode=mode)
         flop_array.append(float(answer['value']))
         Tsnap_array.append(answer[Tsnap])
         flop_results[nfacets] = flop_array[i]/1e15
-        if nfacets >= 3:
-            if (flop_array[i] >= flop_array[i-1]) and  (flop_array[i-1] >= flop_array[i-2]):
+        if nfacets >= 2:
+            if flop_array[i] >= flop_array[i-1]:
                 if verbose:
                     print '\nExpression increasing with number of facets; aborting exploration of Nfacets > %d' % nfacets
                 break
