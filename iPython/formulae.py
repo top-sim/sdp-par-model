c446ea6fa44d7f04c2943d58ccb6a1d420be1151
Francois Malan <fmalan@medvis.org>
2015-03-16 18:00:07 +0200

    Interactive iPython notebook (with sliders) now works - including drawing a pie chart.

--- iPython/formulae.py
+++ iPython/formulae.py
@@ -23,26 +23,26 @@ class Formulae:
         o.Ngw = 2*o.Theta_fov * sqrt((o.Qw2 * o.DeltaW_max**2 * o.Theta_fov**2/4.0)+(o.Qw32 * o.DeltaW_max**1.5 * o.Theta_fov/(o.epsilon_w*pi*2))) #size of the support of the w kernel evaluated at maximum w (Consistent with PDR05 280115)
         o.Ncvff = o.Qgcf*sqrt(o.Naa**2+o.Ngw**2) #The total linear kernel size (Consistent with PDR05 280115)
 
+        o.Nf_no_smear = log(o.wl_max/o.wl_min) / log(3*o.wl/(2*o.Bmax_bin)/(o.Theta_fov*o.Qbw)+1)
+
         # Rrp <= Reprojection Flop rate, per output channel
         if mode == 'Continuum':
-            o.Nf_used  = log(o.wl_max/o.wl_min) / log(3*o.wl/(2*o.Bmax_bin)/(o.Theta_fov*o.Qbw)+1) #Number of channels for gridding at longest baseline
+            o.Nf_used  = o.Nf_no_smear #Number of channels for gridding at longest baseline
             o.Rrp  = o.Nfacet**2 * 50 * o.Npix_linear**2 / o.Tsnap #(Consistent with PDR05 280115)
         elif mode == 'Spectral':
             o.Nf_used  = o.Nf_max
             o.Rrp  = o.Nfacet**2 * 50 * o.Npix_linear**2 / o.Tsnap #(Consistent with PDR05 280115)
         elif mode == 'SlowTrans':
-            o.Nf_used  = log(o.wl_max/o.wl_min) / log(3*o.wl/(2*o.Bmax_bin)/(o.Theta_fov*o.Qbw)+1) #Number of bands for gridding at longest baseline
+            o.Nf_used  = o.Nf_no_smear #Number of bands for gridding at longest baseline
             o.Rrp  = 0*u.s / u.s #(Consistent with PDR05 280115)
         else:
             raise Exception()
 
-        o.Nf_no_smear  = log(o.wl_max/o.wl_min) / log(3*o.wl/(2*o.Bmax_bin)/(o.Theta_fov*o.Qbw)+1)
-        #o.Nf_vis = max(o.Nf_out, o.Nf_no_smear)
-
-        #The following workaround is no longer needed
+        # o.Nf_vis = max(o.Nf_out, o.Nf_used)
+        # The following workaround is (still) needed. Note: should Nf_used be used instead of Nf_no_smear
         o.Nf_vis=(o.Nf_out*sign(floor(o.Nf_out/o.Nf_no_smear)))+(o.Nf_no_smear*sign(floor(o.Nf_no_smear/o.Nf_out))) #Workaround to avoid recursive errors...effectively is Max(Nf_out,Nf_no_smear)
 
-        o.Nvis = o.binfrac*o.Na*(o.Na-1)*o.Nf_used/(2*o.Tdump) * u.s # Number of visibilities per second to be gridded (after averaging short baselines to coarser freq resolution). Note multiplication by u.s to get rid of /s
+        o.Nvis = o.binfrac*o.Na*(o.Na-1)*o.Nf_vis/(2*o.Tdump) * u.s # Number of visibilities per second to be gridded (after averaging short baselines to coarser freq resolution). Note multiplication by u.s to get rid of /s
         o.Rgrid = (o.Nfacet**2 + o.Nfacet)*0.5*8*o.Nmm*o.Nvis*(o.Ngw**2+o.Naa**2) #added Nfacet dependence. Linear becuase there are Nfacet^2 facets but can integrate Nfacet times longer at gridding as fov is lower. Needs revisiting. (Consistent with PDR05 280115)
 
         o.Rccf = o.Nfacet**2 * 5 * o.binfrac *(o.Na-1)* o.Na * o.Nmm * o.Ncvff**2 * log(o.Ncvff,2)/(o.Tion*o.Qfcv) #reduce by multiplication by o.binfrac (RCB), add in extra multiplication by Nfacet-squared.
@@ -51,13 +51,13 @@ class Formulae:
 
         o.Gcorr = o.Na * (o.Na - 1) * o.Nf_used * o.Nbeam * o.Nw * o.Npp / o.Tdump  # Minimum correlator output data rate, after baseline dependent averaging
         o.Mbuf_vis = 2 * o.Mvis * o.Nbeam * o.Npp * o.Nvis * o.Tobs / u.s # Note the division by u.s to get rid of pesky SI unit. Also note the factor 2 -- we have a double buffer (allowing storage of a full observation while simultaneously capturing the next)
-        o.Mw_cache = o.Ngw**3 * o.Qgcf**3 * o.Nbeam * o.Nf_used * 8
+        o.Mw_cache = o.Ngw**3 * o.Qgcf**3 * o.Nbeam * o.Nf_vis * 8
         o.Rio = o.Mvis * (o.Nmajor+1) * o.Nbeam * o.Npp * o.Nvis * o.Nfacet**2 #added o.Nfacet dependence; changed Nmajor factor to Nmajor+1 as part of post PDR fixes.
 
         # Split the FLOP rate into constituent parts, for plotting
         Rflop_common_factor = 2 * o.Nmajor * o.Nbeam * o.Npp
         o.Rflop_grid = Rflop_common_factor * o.Rgrid
-        o.Rflop_conv = Rflop_common_factor * o.Nf_used * o.Rccf
+        o.Rflop_conv = Rflop_common_factor * o.Nf_vis * o.Rccf
         o.Rflop_fft  = Rflop_common_factor * o.Nf_out * o.Rfft
         o.Rflop_proj = Rflop_common_factor * o.Nf_out * o.Rrp
         o.Rflop_phrot = o.Rphrot
