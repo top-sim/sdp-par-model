7f485bc6d666c01bf78dcc27e45be7d1034358dd
Peter Wortmann <pw410@cam.ac.uk>
2019-02-01 12:22:31 +0000

    New attempt at scheduling
    There's a couple of differences here to Francois' scheduler, both
    conceptual and technical. Most important is likely that we use a level
    trace that always returns to zero in order to reason about
    scheduling. This makes it easier to prevent "leaks" from happening,
    and especially means we can guarantee that every task that fits into
    the capacities will be scheduled eventually.
    
    The way this works is that for "edge costs" (e.g. buffer objects) we
    first reserve cost just to the end of the current task, and any future
    dependent task needs to "extend" the edge. If this fails, we know that
    we need to re-schedule the original task past the point where the edge
    extension failed (i.e. the buffer was too full). This leads to a
    comparatively easy back-tracking scheme that eventually places every
    task.
    
    Result is that Low is *barely* realistic, and Mid needs quite a bit
    more buffer and especially Delivery capacity. Will need to check further.

---
+++ sdp_par_model/scheduling/graph.py
@@ -0,0 +1,249 @@
+
+import math
+import uuid
+
+from sdp_par_model import reports
+from sdp_par_model.config import PipelineConfig
+from sdp_par_model.parameters.definitions import \
+    Pipelines, HPSOs, Constants as c
+
+class Task(object):
+
+    def __init__(self, name, result_name, time, cost, edge_cost):
+        """Creates a new task to include in a task graph.
+
+        :param name: Display name for the task
+        :param result_name: Result of this task
+        :param time: Time it takes to evaluate task
+        :param cost: Resources required to run task
+        :param edge_cost: Resources required to support outgoing
+          edges. We assume these resources are required from the time
+          this task starts to the point where the last dependent task
+          finishes.
+        """
+
+        self.id = uuid.uuid4()
+        self.name = name
+        self.result_name = result_name
+        self.time = time
+        self.cost = cost
+        self.edge_cost = edge_cost
+
+        # Dependencies (add afterward)
+        self.deps = set()
+        self.rev_deps = set()
+
+    def __repr__(self):
+        return "Task({}, {}, time={}, cost={}, edge_cost={})".format(
+            repr(self.name), repr(self.result_name), self.time, self.cost, self.edge_cost)
+
+    def depend(self, other):
+        self.deps.add(other)
+        other.rev_deps.add(self)
+
+    def all_cost(self):
+        """ Returns all cost required to run this task. This includes edge costs
+        both of this task and dependencies. """
+        cost_set = set(self.cost.keys()).union(self.edge_cost.keys())
+        for d in self.deps:
+            cost_set = cost_set.union(d.edge_cost.keys())
+
+        return {
+            cost : sum([self.cost.get(cost,0), self.edge_cost.get(cost,0)] +
+                       [d.edge_cost.get(cost,0) for d in self.deps])
+            for cost in cost_set
+            }
+
+    def __hash__(self):
+        return hash(self.id)
+
+class Resources:
+
+    # Compute capacities
+    BatchCompute = "batch-compute" # costing FLOP/s
+    RealtimeCompute = "realtime-compute" # costing FLOP/s
+
+    # Buffer capacities
+    InputBuffer = "input-buffer" # Byte
+    HotBuffer = "hot-buffer" # Byte
+    OutputBuffer = "output-buffer" # Byte
+
+    # Data rates
+    IngestRate = "ingest-rate" # Byte/s
+    ColdBufferRate = "cold-rate" # Byte/s (in + out)
+    HotBufferRate = "hot-rate" # Byte/s (in + out)
+    DeliveryRate = "delivery-rate" # Byte/s (in + out)
+    LTSRate = "lts-rate" # Byte/s (in + out)
+
+    All = [ BatchCompute, RealtimeCompute,
+            InputBuffer, HotBuffer, OutputBuffer,
+            IngestRate, ColdBufferRate, HotBufferRate, DeliveryRate, LTSRate
+    ]
+
+class Lookup:
+    """ Data to extract data from CSV in format of original telescope parameters.
+
+    TODO: Replace this hack by serialising telescope paramameters directly..."""
+
+    Tobs = ('Observation time', 1)
+    Ringest_total = ('Total buffer ingest rate', c.tera)
+    Rio = ('Visibility I/O Rate', c.tera)
+    Rflop = ('Total Compute Requirement', c.peta)
+    Mbuf_vis = ('Visibility Buffer', c.peta)
+    Mw_cache = ('Working (cache) memory', c.tera)
+    Mout = ('Output Size', c.tera)
+    Tpoint = ('Pointing Time', 1)
+    Texp = ('Total Time', 1)
+    Mimage_cube = ('Image cube size', c.giga)
+    Mcal_out = ('Calibration output', c.giga)
+
+def lookup_csv(csv, cfg_name, lookup):
+    """ Lookup (resource) value from CSV
+    :param csv: CSV table as returned by reports.read_csv
+    :param cfg_name: Configuration name to read values for
+    :param resource: Resource name and multiplier
+    """
+
+    name, multiplier = lookup
+    result = float(reports.lookup_csv(csv, cfg_name, name))
+    if result is None:
+        return None
+    return int(math.ceil(multiplier * result))
+
+def make_receive_rt(csv, cfg):
+    """ Generate task(s) for Receive + RT processing.
+
+    Last node returned will have measurement data in cold buffer
+    """
+
+    Tobs = None
+    Rflop = 0
+    pips = []
+    for pipeline in HPSOs.hpso_pipelines[cfg['hpso']]:
+        if pipeline not in Pipelines.realtime:
+            continue
+        pips.append(pipeline)
+        cfg_name = PipelineConfig(pipeline=pipeline, **cfg).describe()
+
+        # Get parameters
+        if pipeline == Pipelines.Ingest:
+            Tobs = lookup_csv(csv, cfg_name, Lookup.Tobs)
+            Ringest = lookup_csv(csv, cfg_name, Lookup.Ringest_total)
+            Mbuf_vis = lookup_csv(csv, cfg_name, Lookup.Mbuf_vis)
+        Rflop += lookup_csv(csv, cfg_name, Lookup.Rflop)
+    assert Tobs is not None, "No ingest pipeline for HPSO {}?".format(cfg['hpso'])
+
+    receive_rt = Task(
+        " + ".join(pips), "Measurements", Tobs,
+        { Resources.RealtimeCompute: Rflop,
+          Resources.ColdBufferRate: Ringest,
+          Resources.IngestRate: Ringest},
+        { Resources.InputBuffer: Mbuf_vis
+            # TODO: RCAL calibration outputs?
+        }
+    )
+    return [receive_rt]
+
+def make_stage_buffer(inp, transfer_rate):
+    """ Generate task(s) for staging data to hot buffer
+
+    Last node returned will have data in hot buffer
+    """
+
+    stage_size = inp.edge_cost[Resources.InputBuffer]
+    stage = Task(
+        "Stage {}".format(inp.result_name), inp.result_name,
+        stage_size / transfer_rate,
+        { Resources.ColdBufferRate: transfer_rate,
+          Resources.HotBufferRate: transfer_rate }, # Some compute for staging resources?
+        { Resources.HotBuffer: stage_size }
+    )
+    stage.depend(inp)
+    return [stage]
+
+def make_offline(csv, cfg, inp, flop_rate, hot_buffer_rate):
+    """Generate task(s) for off-line processing. Task results will be
+    data products.
+
+    :param csv: Performance oracle
+    :param cfg: Pipeline configuration to generate tasks for
+    :param inp: Input task (measurements in hot buffer)
+    :param max_flop_rate: Maximum computational capacity available
+    """
+
+    assert cfg['hpso'] is not None
+
+    # Collect offline configs
+    cfg_time = {}
+    cfg_floprate = {}
+    cfg_iorate = {}
+    cfg_output = {}
+    for pipeline in HPSOs.hpso_pipelines[cfg['hpso']]:
+        if pipeline in Pipelines.realtime:
+            continue
+        cfg_name = PipelineConfig(pipeline=pipeline, **cfg).describe()
+
+        # Get parameters
+        Tobs = lookup_csv(csv, cfg_name, Lookup.Tobs)
+        Rflop = lookup_csv(csv, cfg_name, Lookup.Rflop)
+        Rio = lookup_csv(csv, cfg_name, Lookup.Rio)
+        Mout = lookup_csv(csv, cfg_name, Lookup.Mout)
+
+        # Scale from observation time (Rflop) to computation time
+        # (flop_rate).
+        if pipeline != Pipelines.DPrepD:
+            scale = max(Rflop / flop_rate, Rio / hot_buffer_rate)
+            cfg_iorate[pipeline] = Rio / scale
+        else:
+            scale = Rflop / flop_rate
+        cfg_time[pipeline] = Tobs * scale
+        cfg_floprate[pipeline] = Rflop / scale
+        cfg_output[pipeline] = Mout
+
+    offline = Task(
+        " + ".join(cfg_time.keys()), "Data Products",
+        sum(cfg_time.values()),
+        {
+            Resources.BatchCompute: max(*cfg_floprate.values()),
+            Resources.HotBufferRate: max(*cfg_iorate.values()),
+        }, # Some compute for staging resources?
+        {
+            Resources.OutputBuffer: sum(cfg_output.values())
+        }
+    )
+    offline.depend(inp)
+    return [offline]
+
+def make_deliver(csv, inp, delivery_rate):
+
+    deliver_size = inp.edge_cost[Resources.OutputBuffer]
+    deliver = Task(
+        "Deliver {}".format(inp.result_name), inp.result_name,
+        deliver_size / delivery_rate,
+        { Resources.ColdBufferRate: delivery_rate,
+          Resources.DeliveryRate: delivery_rate },
+        { }
+    )
+    deliver.depend(inp)
+    return [deliver]
+
+def make_graph(csv, cfg,
+               cold_transfer_rate, offline_flop_rate, hot_buffer_rate, delivery_rate):
+    """ Generates complete graph of tasks for an HPSO. """
+
+    # Do receive + realtime processing. If all pipelines associated
+    # with the HPSO are real-time, this is all we need to do
+    receive_rt = make_receive_rt(csv, cfg)
+    if all([ pip in Pipelines.realtime for pip in HPSOs.hpso_pipelines[cfg['hpso']]]):
+        return receive_rt
+
+    # Stage measurements to hot buffer
+    stage = make_stage_buffer(receive_rt[-1], cold_transfer_rate)
+
+    # Do offline processing
+    offline = make_offline(csv, cfg, stage[-1], offline_flop_rate, hot_buffer_rate)
+
+    # Delivery
+    deliver = make_deliver(csv, offline[-1], delivery_rate)
+
+    return receive_rt + stage + offline + deliver
---
+++ sdp_par_model/scheduling/scheduler.py
@@ -0,0 +1,163 @@
+
+from . import level_trace
+
+def toposort(tasks):
+    """Simple topological sort routine for task lists. Not fast, but easy
+    - especially leaves nodes in original order if already topologically
+    sorted. Dependencies that are not in the tasks list are ignored for
+    the purpose of the sort.
+    """
+
+    tasks_set = set(tasks)
+    tasks_out = []
+    tasks_out_set = set()
+    to_do = tasks
+    while len(to_do) > 0:
+        new_to_do = []
+        for t in to_do:
+            if set(t.deps).intersection(tasks_set).issubset(tasks_out_set):
+                tasks_out.append(t)
+                tasks_out_set.add(t)
+            else:
+                new_to_do.append(t)
+        to_do = new_to_do
+    return tasks_out
+
+def active_dependencies(tasks, active):
+    """Walks dependencies of the given task for tasks considered
+    'active'. Note that dependencies of inactive tasks are not searched.
+
+    :param tasks: Iterable of tasks
+    :param active: Set/dictionary of active tasks
+    :return: Topologically sorted list of tasks
+    """
+
+    # If we re-schedule the dependency, other tasks depending
+    # on it might need to get re-scheduled as well
+    repeat = False
+    tasks = set(tasks)
+    work_set = set(tasks)
+    while len(work_set) > 0:
+        next_work_set = set()
+        for d in work_set:
+            for d2 in d.rev_deps:
+                if d2 in active and d2 not in tasks:
+                    tasks.add(d2)
+                    next_work_set.add(d2)
+        work_set = next_work_set
+
+    return toposort(tasks)
+
+def _apply_cost(task, time, usage, remove=False):
+    for cost,amount in task.cost.items():
+        usage[cost].add(time, time + task.time, -amount if remove else amount)
+
+def _apply_edge_cost(task, time, edge_end_time, usage, remove=False):
+    for cost,amount in task.edge_cost.items():
+        usage[cost].add(time, edge_end_time, -amount if remove else amount)
+
+def assert_schedule_consistency(usage, task_time, task_edge_end_time):
+
+    usage_check = { res: level_trace.LevelTrace() for res in usage.keys() }
+    for task in task_time.keys():
+        _apply_cost(task, task_time[task], usage_check)
+        _apply_edge_cost(task, task_time[task], task_edge_end_time[task], usage_check)
+    for cost in usage:
+        assert usage[cost] == usage_check[cost], usage[cost] - usage_check[cost]
+
+def schedule(tasks, capacities, verbose=False):
+    """Schedules a (multi-)graph of tasks in a way that resource usage
+    stays below capacities.
+
+    :param tasks: List of tasks, in topological order
+    :param capacities: Dictionary of resource names to capacity
+    :return: Tuple (usage, task_time, task_edge_end_time)
+    """
+
+    usage = { res: level_trace.LevelTrace() for res in capacities }
+
+    task_constraint = {}
+    task_time = {}
+    task_edge_end_time = {}
+
+    end_of_time = 1e15
+    tasks_to_do = list(tasks)
+    while len(tasks_to_do) > 0:
+        task = tasks_to_do.pop(0)
+
+        # Remove all edge cost for dependencies - we will add them back
+        # once we know where to schedule this task (and therefore how
+        # long the edge needs to be)
+        time = task_constraint.get(task, 0)
+        for d in task.deps:
+            time = max(time, task_time[d] + d.time)
+            _apply_edge_cost(d, task_time[d], task_edge_end_time[d], usage, remove=True)
+
+        # Find suitable start time. Given the above check this must succeed eventually,
+        # because all levels return to zero.
+        finished = False
+        while not finished:
+            finished = True
+            for cost, amount in task.all_cost().items():
+                new_time = usage[cost].find_period_below(time, end_of_time, capacities[cost] - amount, task.time)
+                if new_time > time:
+                    if verbose:
+                        print("{} for {} ({} s): Amount {} @ {}".format(
+                            cost,task.name, task.time, amount, new_time))
+                    time = new_time
+                    finished = False
+
+        # Alright, now check that we can extend the edges
+        task_end_time = time + task.time
+        need_to_reschedule = set()
+        for d in task.deps:
+            # Elongate edge, if needed
+            task_edge_end_time[d] = max(task_edge_end_time[d], task_end_time)
+            # Check that we have enough capacity to do this
+            for cost,amount in d.edge_cost.items():
+                max_use = usage[cost].maximum(task_time[d], task_edge_end_time[d])
+                # Over maximum usage? Note, but add anyway so we don't need to
+                # remember what costs we added
+                if max_use + amount > capacities[cost]:
+                    task_constraint[d] = usage[cost].find_above_backward(time,
+                                                                         capacities[cost] - amount)
+                    assert usage[cost].get(task_constraint[d]) <= capacities[cost] - amount
+                    if verbose:
+                        print("{} for {} dependency {} ({} s): overflow ({}+{}), "
+                              "need to reschedule past {}".format(
+                                  cost,task.name,d.name, task_time[d],
+                                  max_use, amount, task_constraint[d]))
+                    need_to_reschedule.add(d)
+            _apply_edge_cost(d, task_time[d], task_edge_end_time[d], usage)
+
+        # Needs re-scheduling of dependencies?
+        if len(need_to_reschedule) > 0:
+
+            # Reschedule tasks, plus previously-scheduled dependencies
+            reschedule_list = active_dependencies(need_to_reschedule, task_time)
+            if verbose:
+                print("Rescheduling", ", ".join([d.name for d in reschedule_list]))
+            for t in reschedule_list:
+                _apply_cost(t, task_time[t], usage, remove=True)
+                _apply_edge_cost(t, task_time[t], task_edge_end_time[t], usage, remove=True)
+                del task_time[t]
+                del task_edge_end_time[t]
+
+            # Add to front of list
+            reschedule_list.append(task)
+            tasks_to_do = reschedule_list + tasks_to_do
+            continue
+
+        # Finally, add the task itself
+        if verbose:
+            print("=> {}: {}-{}".format(task.name, time, task_end_time))
+        task_time[task] = time
+        task_edge_end_time[task] = task_end_time
+        _apply_cost(task, task_time[task], usage)
+        _apply_edge_cost(task, task_time[task], task_edge_end_time[task], usage)
+
+    # Consistency check
+    if verbose:
+        assert_schedule_consistency(usage, task_time, task_edge_end_time)
+
+    return usage, task_time, task_edge_end_time
