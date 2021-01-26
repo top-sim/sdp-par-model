4b460ac57df1e3bd4b484155fad2aca92cdf8578
Peter Wortmann <pw410@cam.ac.uk>
2021-01-26 11:09:29 +0000

    Fix setup.py
    README doesn't exist (any more?)

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 setup(name='sdp-par-model',
       version='0.6',
       description='SDP Parameteric Model',
-      long_description=open('README').read(),
+      long_description=open('README.md').read(),
       author='Rosie Bolton, Francois Malan, Bojan Nikolic, Andreas Wicenec, Peter Wortmann',
       url='https://github.com/SKA-ScienceDataProcessor/sdp-par-model',
       license='Apache License Version 2.0',
-      packages=['sdp_par_model'],
+      packages=['sdp_par_model', 'sdp_par_model.parameters', 'sdp_par_model.scheduling' ],
       test_suite="tests",
       tests_require=['pytest'],
       )
